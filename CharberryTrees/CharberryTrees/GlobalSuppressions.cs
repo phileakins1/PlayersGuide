39bed98fc2f9dd5e6cd0769a90c3d913115ac693
Phil Eakins <phileakins1@talktalk.net>
2024-10-14 16:21:32 +0100

    Added Charberry Tree Challenge

---
+++ CharberryTrees/CharberryTrees/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "<Pending>", Scope = "member", Target = "~M:Harvester.#ctor(CharberryTree)")]
+[assembly: SuppressMessage("Minor Code Smell", "S1481:Unused local variables should be removed", Justification = "<Pending>")]
---
+++ CharberryTrees/CharberryTrees/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+CharberryTree tree = new();
+Notifier notifier = new(tree);
+Harvester harvester = new(tree);
+
+
+while (true)
+{
+    tree.MayBeGrow();
+}
+
+
+public class Notifier
+{
+    private static void OnRipened() => Console.WriteLine("The Charberry fruit has ripened.");
+
+    public Notifier(CharberryTree tree)
+    {
+        tree.Ripened += OnRipened;
+    }
+}
+
+public class Harvester
+{
+    private static CharberryTree? Tree { get; set; }
+    private static void OnRipened()
+    {
+        Tree!.Ripe = false;
+        Console.WriteLine("The fruit has been harvested.");
+    }
+
+    public Harvester(CharberryTree tree)
+    {
+        Tree = tree;
+        tree.Ripened += OnRipened;
+    }
+}
+
+public class CharberryTree
+{
+    private readonly Random _random = new();
+
+    public event Action? Ripened;
+    public bool Ripe { get; set; }
+
+    public void MayBeGrow()
+    {
+        if (_random.NextDouble() < 0.00000001)
+        {
+            Ripe = true;
+            Ripened();
+        }
+    }
+}
